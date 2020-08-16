bef3e46255fea9bae866cb47166f712868352df1
hugo <hugogher@gmail.com>
2020-08-16 11:10:06 -0300

    se agrega heroeComponent, junto con sus rutas

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service' ;
 import { IHeroe } from 'src/app/models/IHeroe';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,19 @@ export class HeroesComponent implements OnInit {
 
   heroes: IHeroe[] = [];
 
-  constructor(private heroesService: HeroesService) { }
+  constructor(private heroesService: HeroesService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.heroes = this.heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(idHeroe: number): void{
+
+    console.log(idHeroe);
+    this.router.navigate( ['/heroe', idHeroe] );
+
   }
 
 
